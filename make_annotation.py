0c6a2a1df26692d137931b1c5aaaad2d5c993c2a
fbckdgns3@naver.com <fbckdgns3@naver.com>
2024-07-04 11:16:27 +0900

    docs: annotation 파일 수정

--- make_annotation.py
+++ make_annotation.py
@@ -3,10 +3,31 @@ import json
 
 videos_data = [
     {
-        "video_file": "video1.mp4",
+        "video_file": "0001.mp4",
         "annotations": [
-            {"frame": "video1_frame_001.jpg", "label": "answer"},
-            {"frame": "video1_frame_002.jpg", "label": "wrong"}
+            {"frame": "frame_0.jpg", "label": "answer"},
+            {"frame": "frame_1.jpg", "label": "answer"},
+            {"frame": "frame_2.jpg", "label": "answer"},
+            {"frame": "frame_3.jpg", "label": "answer"},
+            {"frame": "frame_4.jpg", "label": "answer"},
+            {"frame": "frame_5.jpg", "label": "answer"},
+            {"frame": "frame_6.jpg", "label": "answer"},
+            {"frame": "frame_7.jpg", "label": "answer"},
+            {"frame": "frame_8.jpg", "label": "answer"},
+            {"frame": "frame_9.jpg", "label": "answer"},
+            {"frame": "frame_10.jpg", "label": "answer"},
+            {"frame": "frame_11.jpg", "label": "answer"},
+            {"frame": "frame_12.jpg", "label": "answer"},
+            {"frame": "frame_13.jpg", "label": "answer"},
+            {"frame": "frame_14.jpg", "label": "answer"},
+            {"frame": "frame_15.jpg", "label": "answer"},
+            {"frame": "frame_16.jpg", "label": "answer"},
+            {"frame": "frame_17.jpg", "label": "answer"},
+            {"frame": "frame_18.jpg", "label": "answer"},
+            {"frame": "frame_19.jpg", "label": "answer"},
+            {"frame": "frame_20.jpg", "label": "answer"},
+            {"frame": "frame_21.jpg", "label": "answer"}
+
         ]
     },
     {
